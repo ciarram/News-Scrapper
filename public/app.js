a939646dad2b7d384a01079b1e5decd7b5193b73
Ciarra Murphy <mciarra@ymail.com>
2017-10-22 16:47:22 -0400

    added formatting to cards and btns

--- public/app.js
+++ public/app.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
     $(".saved-articles").hide();
 
-    $("#scrape").on("click", function(){
+    $("#scrape").on("click", function(event){
         console.log("clicked!");
         alert("Scrape Complete!")
         $.getJSON("/articles", function(data){
             for (var i = 0; i < data.length; i++){
-                $(".results").append('<div class = "card"><div class="card-body">' + data[i]._id + data[i].title + '<input class="btn" type="button" value="Save Article">' + '<br>' + data[i].summary + '</div></div><br>')
-                $(".results").append('')
+                $(".results").append('<div class = "card"><div class="card-body">' + data[i].title + '<input class="btn" type="button" id="save" value="Save Article">' + '<br>' + data[i].summary + '</div></div><br>')
             }
         })
     })
 
+    $("#save").on("click", function(event){
+        event.preventDefault();
+
+    })
 })
